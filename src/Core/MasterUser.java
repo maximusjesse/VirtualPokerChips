8855f5c5a81a3cf85fe325fc51732f7f066d8846
Mark Qi <markqi92@gmail.com>
2014-04-12 16:47:19 -0400

    Adding additional classes
    Created MasterUser and Jackpot classes

---
+++ src/Core/MasterUser.java
@@ -0,0 +1,9 @@
+public class MasterUser extends User {
+	// String userID
+	// float points
+	// float pointsPushed
+	public MasterUser(String userID, float points) {
+		super(userID, points);
+	}
+
+}
