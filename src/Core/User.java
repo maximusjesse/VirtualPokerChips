54528410e64a3bcca44da296aa1e55825f1b1675
maximusjesse <baller17@gmail.com>
2014-04-12 16:07:19 -0400

    User class updated
    Added private variables and methods to User class

--- src/Core/User.java
+++ src/Core/User.java
@@ -4,5 +4,31 @@
 package Core;
 
 public class User {
+	
+	private String userID;
+	private float points, pointsPushed;
+	
+	/*
+	 * Gets user ID
+	 * @return String Returns user ID
+	 */
+	public String getUserID() {
+		return userID;		
+	}
+	
+	/*
+	 * Sets user id
+	 * @param id String identifier to set user id
+	 */
+	public void setUserID(String id) {
+		this.userID = id;	
+	}
+	
+	/*
+	 * Gets the amount of points user currently has	
+	 */
+	public float getPoints() {
+		return points;
+	}
 
 }
